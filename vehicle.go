9671a63b960dc65da7175f9789681f874532d0f1
Fernando Vazquez <vzs.fernando@gmail.com>
2022-04-07 14:42:29 -0300

    module1

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,41 @@
 package main
 
-import (
-
-)
-
+type vehicle interface{}
+type car struct {
+	model       string
+	make        string
+	typeVehicle string
+}
+type bike struct {
+	model string
+	make  string
+}
+type truck struct {
+	model       string
+	make        string
+	typeVehicle string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json:"values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feeback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal    int
+	feebackPositive  int
+	feedbackNeutral  int
+	feedbackNegative int
+}
+
+var vehicleResult map[string]feedbackResult
+var inventory vehicle
 
 type rating float32
 
@@ -21,7 +49,6 @@ const (
 
 func init() {
 
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,15 +61,15 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
